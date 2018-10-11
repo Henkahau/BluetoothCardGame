b27a1b2628ed081b8cbbfe46d4fc865aa284ce50
Unknown <velimatti92@hotmail.com>
2018-10-11 14:48:23 +0300

    poistettu turhat virheelliset importit

--- app/src/main/java/one/group/bluetoothcardgame/BtMessageActivity.java
+++ app/src/main/java/one/group/bluetoothcardgame/BtMessageActivity.java
@@ -11,8 +11,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
